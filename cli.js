6a9223bf7549be02c45f24f532d74cf1f900282a
zepatrik <patrik@ory.sh>
2020-10-24 16:20:45 +0200

    chore: format

--- cli.js
+++ cli.js
@@ -1,21 +1,23 @@
-const { argv, exit } = require("process");
-const { bwo } = require("./src/fetchers/index")
+const { argv, exit } = require('process')
+const { bwo } = require('./src/fetchers/index')
 
 var allCases = []
 const registerCase = (c) => {
-    allCases = [...allCases, c]
-    return c
+  allCases = [...allCases, c]
+  return c
 }
 
 const main = async () => {
-    switch (argv[2]) {
-        case registerCase("bwo"):
-            await bwo().then(console.log)
-            break
-        default:
-            console.error(`location ${argv[2]} is unknown, use one of ${JSON.stringify(allCases)}`)
-            exit(1)
-    }
+  switch (argv[2]) {
+    case registerCase('bwo'):
+      await bwo().then(console.log)
+      break
+    default:
+      console.error(
+        `location ${argv[2]} is unknown, use one of ${JSON.stringify(allCases)}`
+      )
+      exit(1)
+  }
 }
 
 main()
--- src/fetchers/index.js
+++ src/fetchers/index.js
@@ -1,3 +1,3 @@
 module.exports = {
-    bwo: require("./bwo")
+  bwo: require('./bwo')
 }
