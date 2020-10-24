ac7de7b14ad6510ee6620ddbc4fc7d18977f4868
Leon Kiesgen <leon.kiesgen@web.de>
2020-10-24 16:58:19 +0200

    adding main run script

--- src/writeToFolder.js
+++ src/writeToFolder.js
@@ -1,9 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
+const defaultOccupancy = {
+  count: 0,
+  maximum: 0,
+  percent: 0,
+  queue: 0
+}
 // add json to correct Filepath
 function writeOccupancy(occupancy, location) {
-  date = JSON.stringify(occupancy)
   const timestamp = new Date(
     new Date().toLocaleString('en-us', { timeZone: 'Europe/Berlin' })
   )
@@ -13,15 +18,13 @@ function writeOccupancy(occupancy, location) {
   const filename = [year, month, day].join('-')
   const dir = path.join('data', location)
   const filepath = path.join(dir, filename + '.json')
-  console.log(filepath)
-  console.log(fs.existsSync(filepath))
   if (fs.existsSync(filepath)) {
     return fs.promises
       .readFile(filepath, 'utf-8')
       .then((data) => {
         const array = JSON.parse(data.toString())
-        array.push({ ...occupancy, timestamp: timestamp })
-        return JSON.stringify(array)
+        array.push({ ...defaultOccupancy, ...occupancy, timestamp: timestamp })
+        return JSON.stringify(array, null, 2)
       })
       .then((data) => {
         fs.promises.writeFile(filepath, data)
@@ -30,8 +33,7 @@ function writeOccupancy(occupancy, location) {
     fs.promises.mkdir(dir, { recursive: true }, (err) => {
       if (err) throw err
     })
-    const data = [{ ...occupancy, timestamp: timestamp }]
-    console.log(JSON.stringify(data))
+    const data = [{ ...defaultOccupancy, ...occupancy, timestamp: timestamp }]
     return fs.promises.writeFile(
       filepath,
       JSON.stringify(data, null, 2),
