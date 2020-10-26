2c7b11ecf8c99281cb73f38b73fa660c5aa35d0d
Leon Kiesgen <leon.kiesgen@web.de>
2020-10-26 19:44:45 +0100

    adding einstein fetcher

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const writeOccupancy = require('./src/writeToFolder')
-const { bwo, swm } = require('./src/fetchers/index')
+const { bwo, swm, bwei } = require('./src/fetchers/index')
 const swmMapping = require('./src/helpers/swm_mapping')
 
 // run fetcher
 async function run() {
   await bwo().then((data) => writeOccupancy(data, 'bwo'))
+  await bwei().then((data) => writeOccupancy(data, 'bwei'))
   await swm().then((data) =>
     Promise.all(
       data.map(({ swmId, ...rest }) =>
---
+++ src/fetchers/bwei.js
@@ -0,0 +1,20 @@
+const rp = require('request-promise')
+const $ = require('cheerio')
+
+const fetcher = () =>
+  rp(
+    'https://www.boulderado.de/boulderadoweb/gym-clientcounter/index.php?mode=get&token=eyJhbGciOiJIUzI1NiIsICJ0eXAiOiJKV1QifQ.eyJjdXN0b21lciI6IkVpbnN0ZWluTSJ9.uH9xRoVykz5fzofHc-JGigeHreaeTayel49o3FR6cNA'
+  )
+    .then((html) => {
+      const count = parseInt(
+        $('div.actcounter-content', html).children().text()
+      )
+      const free = parseInt(
+        $('div.freecounter-content', html).children().text()
+      )
+      const maximum = count + free
+      const percent = (count / maximum) * 100
+      return { count, percent, maximum }
+    })
+    .then((data) => data)
+module.exports = fetcher
