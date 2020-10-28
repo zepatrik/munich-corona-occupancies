134149c1043ada5ddb34bc4f234c7ce90567235b
zepatrik <patrik@ory.sh>
2020-10-28 23:58:50 +0100

    fix: calibrate maximum number of people

--- src/helpers/dav_mapping.js
+++ src/helpers/dav_mapping.js
@@ -1,10 +1,10 @@
 module.exports = [
-  { id: 'k_dav_toelz', maximum: 1000 },
-  { id: 'b_dav_toelz', maximum: 1000 },
-  { id: 'k_dav_thalk', maximum: 1000 },
-  { id: 'b_dav_thalk', maximum: 1000 },
-  { id: 'k_dav_gilch', maximum: 1000 },
-  { id: 'b_dav_gilch', maximum: 1000 },
-  { id: 'k_dav_freim', maximum: 1000 },
-  { id: 'b_dav_freim', maximum: 1000 }
+  { id: 'k_dav_toelz', maximum: 58 },
+  { id: 'b_dav_toelz', maximum: 55 },
+  { id: 'k_dav_thalk', maximum: 138 },
+  { id: 'b_dav_thalk', maximum: 56 },
+  { id: 'k_dav_gilch', maximum: 69 },
+  { id: 'b_dav_gilch', maximum: 17 },
+  { id: 'k_dav_freim', maximum: 67 },
+  { id: 'b_dav_freim', maximum: 95 }
 ]
