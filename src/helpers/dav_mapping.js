f7beb7c3028142120fe87d3d4f8836ad4f10018c
zepatrik <patrik@ory.sh>
2020-10-29 00:03:34 +0100

    fix: dav calibration

--- src/helpers/dav_mapping.js
+++ src/helpers/dav_mapping.js
@@ -1,10 +1,10 @@
 module.exports = [
-  { id: 'k_dav_toelz', maximum: 58 },
-  { id: 'b_dav_toelz', maximum: 55 },
-  { id: 'k_dav_thalk', maximum: 138 },
-  { id: 'b_dav_thalk', maximum: 56 },
-  { id: 'k_dav_gilch', maximum: 69 },
-  { id: 'b_dav_gilch', maximum: 17 },
-  { id: 'k_dav_freim', maximum: 67 },
-  { id: 'b_dav_freim', maximum: 95 }
+  { id: 'k_dav_toelz', maximum: 66 },
+  { id: 'b_dav_toelz', maximum: 65 },
+  { id: 'k_dav_thalk', maximum: 177 },
+  { id: 'b_dav_thalk', maximum: 72 },
+  { id: 'k_dav_gilch', maximum: 80 },
+  { id: 'b_dav_gilch', maximum: 24 },
+  { id: 'k_dav_freim', maximum: 98 },
+  { id: 'b_dav_freim', maximum: 120 }
 ]
